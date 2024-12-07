8090f3605abd991dfc3037ce4f731e1c205599ec
david-walters <dawudwalters@gmail.com>
2024-12-07 21:56:12 +0000

    Update views.py to be able to render all the perfume objects from the model

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .models import Perfume
 
 def index(request):
-    return render(request, 'index.html')
+    perfumes = Perfume.objects.all()
+    return render(request, 'index.html', {'perfumes': perfumes})
 
 def register(request):
     if request.method == 'POST':
