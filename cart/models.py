6ac166c4e01013e033b24b43201a9e63c43623b5
david-walters <dawudwalters@gmail.com>
2024-12-15 23:00:19 +0000

    Add a total cost in the cart model to be able do make a total cost of all shopping cart items to finalise the purchase

--- cart/models.py
+++ cart/models.py
@@ -8,6 +8,11 @@ class CartItem(models.Model):
     perfume = models.ForeignKey(Perfume, on_delete=models.CASCADE)
     size = models.CharField(max_length=10)
     quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=24.00)
+
+    def save(self, *args, **kwargs):
+        self.total_price = 24 * self.quantity
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.perfume.name} ({self.size}) x {self.quantity}"
